6e028e1a75d5f60c206d8a6b151aa0e8377463cb
jack-pepper <70756191+jack-pepper@users.noreply.github.com>
2024-11-22 11:42:30 +0100

    manage common cases, save before testing edge cases

--- get_next_line.c
+++ get_next_line.c
@@ -30,7 +30,10 @@ int main(void)
 		printf("=== ITERATION %d === \n", i);
 		next_line = get_next_line(fd);
 		if (next_line == NULL)
+		{
+			printf("End of file!\n");
 			break ;
+		}
 		printf("Final next_line: %s\n", next_line);
 		free(next_line);
 	}
@@ -44,25 +47,30 @@ char	*get_next_line(int fd)
 	static char	*stash = NULL;
 	int		found_line;
 	char		*cursor; // is actually useless. Or could be passed to seize_line to be used there
+	int		res;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
 		return (NULL);
-	printf("[1] Buf successfully mallocated!\n");
+	//printf("[1] Buf successfully mallocated!\n");
 	ft_memset(buf, 0, BUFFER_SIZE + 1);
 	next_line = NULL;
 	cursor = NULL;
 	found_line = 0;
 	while (!found_line)
 	{
-		if (read_and_stash(fd, buf, &stash) == 0)
+		res = read_and_stash(fd, buf, &stash);
+		if (res == -1)
 			return (NULL);
-		cursor = ft_strchr(stash, '\n');
+		else if (res == 1)
+			cursor = ft_strchr(stash, '\n');
+		else if (res == 2)
+			cursor = NULL;
 	//	printf("cursor: %p\n", cursor);
 	//	printf("Before the cursor loop...\n");
-		if (cursor != NULL)
+		if (cursor != NULL || res == 2)
 		{
 	//		printf("We enter the cursor loop...\n");
 			next_line = seize_line(next_line, &stash, cursor);
@@ -75,54 +83,29 @@ char	*get_next_line(int fd)
 	return (next_line);
 }
 
-char	*seize_line(char *next_line, char **stash, char *cursor)
-{
-	int	chunk_len;
-//	int	i;
-	char	*leftover;
-
-	chunk_len = (cursor - *stash) + 1;
-	printf("[5] chunk_len = %d\n", chunk_len);
-	next_line = malloc(sizeof(char) * (chunk_len + 1));
-	if (!next_line)
-		return (NULL);
-//	i = 0;
-	/*
-	while ((i < chunk_len) && ((*stash)[i] != '\0'))
-	{
-		next_line[i] = (*stash)[i];
-		i++;
-	}
-	next_line[i] = '\0';
-	*/
-	ft_strlcpy(next_line, *stash, chunk_len + 1);
-	//leftover = &(*stash)[i];
-	leftover = cursor + 1;
-	//ft_memset(*stash, 0, i);
-	*stash = leftover;
-	printf("[5-2] next_line: %s\n", next_line);
-	printf("[5-3] stash: %s\n", *stash);
-	return (next_line);
-}
-
 int	read_and_stash(int fd, char *buf, char **stash)
 {
 	size_t	b_read;
 
 	b_read = read(fd, buf, BUFFER_SIZE);
-	printf("[2] value of b_read = %ld\n", b_read);
+	//printf("[2] value of b_read = %ld\n", b_read);
 	if (b_read <= 0)
 	{
-		printf("[2-1] No bytes read!\n");
-		return (0);
+	//	printf("[2-1] No bytes read!\n");
+		if (*stash[0] != '\0')
+		{
+	//		printf("We enter the no bytes loop...\n");
+			return (2);
+		}
+		return (-1);
 	}
 	buf[b_read] = '\0';
 	*stash = stash_manager(stash, buf, b_read);
 	if (!*stash)
-		return (0);
+		return (-1);
 	return (1);
 	// DEBUG
-	printf("[after reading and stash] Stash: %s\n", *stash); 
+	//printf("[after reading and stash] Stash: %s\n", *stash); 
 	//printf("Stash[0]: %02x\n", (unsigned char)(*stash)[0]);
 	//printf("Stash[1]: %02x\n", (unsigned char)(*stash)[1]);
 	//printf("Stash[b_read - 1]: %02x\n", (unsigned char)(*stash)[b_read - 1]);
@@ -133,7 +116,7 @@ char	*stash_manager(char **stash, char *buf, size_t b_read)
 	char	*new_stash;
 	int	stash_len;
 
-	printf("[4] Entering the function stash_manager: %p\n", *stash);
+	//printf("[4] Entering the function stash_manager: %p\n", *stash);
 	if (!*stash)
 	{
 		*stash = malloc(sizeof(char) * (b_read + 1));
@@ -142,15 +125,15 @@ char	*stash_manager(char **stash, char *buf, size_t b_read)
 		ft_memset(*stash, 0, b_read + 1); // might be useless
 		ft_strlcpy(*stash, buf, b_read + 1);
 		(*stash)[b_read] = '\0';
-		printf("[4-new] Stash successfully mallocated and initialized! : %d\n", (*stash)[3]);
+	//	printf("[4-new] Stash successfully mallocated and initialized! : %d\n", (*stash)[3]);
 		return (*stash);
 	}
 	stash_len = ft_strlen(*stash);
-	printf("[4-realloc] stash_len: %d", stash_len);
+	//printf("[4-realloc] stash_len: %d\n", stash_len);
 	new_stash = malloc(sizeof(char) * (stash_len + b_read + 1));
 	if (new_stash == NULL)
 		return (NULL);
-	printf("[4-realloc success] New stash malloc success!");
+	//printf("[4-realloc success] New stash malloc success!\n");
 	ft_memset(new_stash, 0, stash_len + b_read + 1);
 	ft_strlcpy(new_stash, *stash, stash_len + 1);
 	ft_strlcpy(new_stash + stash_len, buf, b_read + 1);
@@ -158,3 +141,39 @@ char	*stash_manager(char **stash, char *buf, size_t b_read)
 	*stash = new_stash;
 	return (*stash);
 }
+
+char	*seize_line(char *next_line, char **stash, char *cursor)
+{
+	int	chunk_len;
+	char	*leftover;
+	int	i;
+
+	//printf("We enter seize_line...\n");
+	i = 0;
+	if (cursor == NULL)
+	{
+		cursor = *stash;
+	//	printf("Cursor: %p - Stash: %p\n", cursor, *stash);
+		while ((*stash)[i] != '\0')
+		{
+			cursor++;
+			i++;
+		}
+	//	printf("Cursor: %p - Stash: %p\n", cursor, *stash);
+	}
+	chunk_len = (cursor - *stash + 1);
+	printf("[5] chunk_len = %d\n", chunk_len);
+	next_line = malloc(sizeof(char) * (chunk_len + 1));
+	if (!next_line)
+		return (NULL);
+	ft_strlcpy(next_line, *stash, chunk_len + 1);
+	leftover = &(*stash)[i];
+	leftover = cursor + 1;
+	//ft_memset(*stash, 0, i);
+	*stash = leftover;
+	printf("[5-2] next_line: %s\n", next_line);
+	printf("[5-3] stash: %s\n", *stash);
+	return (next_line);
+}
+
+
